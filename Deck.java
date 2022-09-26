308834ca6bad2664637ff3a9b2d6f168694eb0c7
dshumbanhete <100418667+dshumbanhete@users.noreply.github.com>
2022-09-26 23:02:29 +0200

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -18,7 +18,29 @@ public class Deck extends CardCollection {
 	}
 	
 	public void riffleShuffle() {
-		
+		int count=0;
+		Card[] gill=new int[15];
+		Card[] fin=new int[15];
+		Card[] eye=new int[22];
+		for (int go=0; go<52; go++){
+			if (go<=14){
+				gill[go]=cards.getCard(go); //thank you for the getter Tess :}
+			}else if (go>29){
+				eye[go-29]=cards.getcard(go);
+			}else{
+				fin[go-14]=cards.getCard(go);
+			}
+		while(count<52){
+			if (count<15){
+				cards.setCards(count, gill[count]);
+				count+=1;
+			}else if (count>36){
+				cards.setCards(count, eye[count-36]);
+				count+=1;
+			}else{
+				cards.setcards(count, fin[count-15]);
+			}
+		}
 	}
 	
 	public void dealCard(CardCollection hand) {
