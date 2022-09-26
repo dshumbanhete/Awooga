f012a94dd433eb22e76eb3656f68e77f7f37d1a3
dshumbanhete <100418667+dshumbanhete@users.noreply.github.com>
2022-09-26 23:18:05 +0200

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,12 +1,12 @@
 public class Deck extends CardCollection {
 	private int numDecks;
-	
+	/**A value constructor for our class*/
 	public Deck(int n) {
 		super("Deck");
 		this.numDecks = n;
 		populate();
 	}
-	
+	/**populate() is a method that constructs our deck from the card collection specified in the parent class*/
 	private void populate() {
 		for (int d = 0; d < numDecks; d++) { //d = deck
 			for (int s = 0; s < 4; s++) { //s = suit
@@ -16,7 +16,7 @@ public class Deck extends CardCollection {
 			}
 		}	
 	}
-	
+	/**riffleshuffle() is a method that is used to shuffle our deck object to ensure that the card objects therein are randomised*/
 	public void riffleShuffle() {
 		int count=0;
 		Card[] gill=new int[15];
@@ -32,13 +32,13 @@ public class Deck extends CardCollection {
 			}
 		while(count<52){
 			if (count<15){
-				cards.setCards(count, gill[count]);
+				cards.set(count, gill[count]);
 				count+=1;
 			}else if (count>36){
-				cards.setCards(count, eye[count-36]);
+				cards.set(count, eye[count-36]);
 				count+=1;
 			}else{
-				cards.setcards(count, fin[count-15]);
+				cards.set(count, fin[count-15]);
 			}
 		}
 	}
