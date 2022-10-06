539599f79cd67db02de2373cd9a490fd27dc33fb
dshumbanhete <100418667+dshumbanhete@users.noreply.github.com>
2022-10-06 13:47:39 +0200

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -2,8 +2,10 @@ public class Hand extends CardCollection {
 	public Hand(String name) {
 		super(name);
 	}
-	
+	/*
 	public int handValue() {
 		return 0;
 	}
+	I'm struggling to see a potential use for this method, especially as it is set to return 0 currently
+	**/ 
 }
